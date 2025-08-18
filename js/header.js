ebe191e569d3dd22cba922cb353c7f38e09ae97e
SoSoSammy <86075091+SoSoSammy@users.noreply.github.com>
2025-08-17 20:38:32 -0600

    Add smooth scrolling

--- js/header.js
+++ js/header.js
@@ -1,5 +1,6 @@
 const headerMenu = document.querySelector(".header__menu");
 const headerNav = document.querySelector(".header__nav");
+const navLinks = document.querySelector(".nav__links");
 
 headerMenu.addEventListener("click", function (e) {
 	// Prevent default behavior of link
@@ -10,4 +11,14 @@ headerMenu.addEventListener("click", function (e) {
 	headerNav.classList.toggle("open");
 	// Prevent scrolling
 	document.body.classList.toggle("no-scroll");
+});
+
+// Implement smooth scrolling
+navLinks.addEventListener("click", function (e) {
+	e.preventDefault();
+
+	if (e.target.classList.contains("nav__link")) {
+		const id = e.target.getAttribute("href");
+		document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+	}
 });
